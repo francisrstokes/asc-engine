71cdea79ec9a7e0161c6579caf56506a80159d75
Frank Stokes <frank@wearereasonablepeople.com>
2019-02-01 13:38:04 +0100

    Export user facing objects in index

---
+++ src/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  AnimatedTile: require('./AnimatedTile'),
+  Animation: require('./Animation'),
+  Area: require('./Area'),
+  constants: require('./constants'),
+  Game: require('./Game'),
+  Tile: require('./Tile'),
+  util: require('./util')
+};
